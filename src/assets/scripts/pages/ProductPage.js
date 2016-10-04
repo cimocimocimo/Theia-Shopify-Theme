a19d5880c544a73c9f852c40b87d46ee62b54843
Aaron Cimolini <aaron@cimolini.com>
2016-10-03 19:05:51 -0700

    Adds class for product page logic.

---
+++ src/assets/scripts/pages/ProductPage.js
@@ -0,0 +1,18 @@
+'use strict';
+
+class ProductPage {
+
+}
+
+var elements = {
+    $body: window.jQuery('.template-product'),
+    $mobileHeaderLink: window.jQuery('#mobileProductHeaderLink'),
+    $infoBlock: window.jQuery('#productInfoBlock'),
+    $descriptionToggle: window.jQuery('#descriptionToggle'),
+    $productOptions: window.jQuery('.product-options-block'),
+    gallery: {
+        $mainImageBlock: window.jQuery('#mainImageBlock'),
+        $thumbBlock: window.jQuery('#productThumbs')
+    },
+    $backLink: window.jQuery('.product-back-link')
+};
